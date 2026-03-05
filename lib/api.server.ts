683f9927a9fca44c024308dcaba137468a0af402
Veljko <veljko_naumovic@yahoo.com>
2026-03-05 18:49:36 +0100

    test variables on server

--- lib/api.server.ts
+++ lib/api.server.ts
@@ -5,6 +5,9 @@ const baseUrl = process.env.VERCEL_URL
 	? `https://${process.env.VERCEL_URL}`
 	: "http://localhost:3000";
 
+console.log("BASE URL:", baseUrl);
+console.log("VERCEL_URL:", process.env.VERCEL_URL);
+
 export const getBookingById = async (id: string): Promise<Booking | null> => {
 	const res = await fetch(`${baseUrl}/api/bookings?id=${id}`, {
 		cache: "no-store",
