92ba8d05bf740a00807bce51337d67163ad41d6b
Veljko <veljko_naumovic@yahoo.com>
2026-03-05 19:36:51 +0100

    Change get booking by id

--- lib/api.server.ts
+++ lib/api.server.ts
@@ -5,21 +5,32 @@ import { Booking } from "@/types/booking";
 export const getBookingById = async (id: string): Promise<Booking | null> => {
 	try {
 		const headersList = await headers();
-		const host = headersList.get("host");
 
-		if (!host) return null;
+		const host =
+			headersList.get("x-forwarded-host") || headersList.get("host");
 
-		const protocol = host.includes("localhost") ? "http" : "https";
+		if (!host) {
+			console.error("HOST not found");
+			return null;
+		}
+
+		const protocol = process.env.VERCEL ? "https" : "http";
 
 		const url = `${protocol}://${host}/api/bookings?id=${id}`;
+
+		console.log("FETCH URL:", url);
 
 		const res = await fetch(url, {
 			cache: "no-store",
 		});
 
+		console.log("RESPONSE STATUS:", res.status);
+
 		if (!res.ok) return null;
 
-		const booking = await res.json();
+		const booking: Booking = await res.json();
+
+		console.log("BOOKING FROM API:", booking);
 
 		return booking;
 	} catch (error) {
